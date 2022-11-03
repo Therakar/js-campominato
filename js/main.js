91ef366ad2af11ff470b7ff372922e94474845e9
Therakar <matteo.pau200@gmail.com>
2022-11-03 12:27:54 +0100

    aggiunge un array vuoto che viene riempito dai numeri scelti e un ciclo if che ha come condizione che il numero premuto sia presente nell'array "chosenNumbers" e nel caso in cui la condizione fosse soddisfatta rimuove l'event listener

--- js/main.js
+++ js/main.js
@@ -1,24 +1,35 @@
 "use strict"
 
-const boardContainer = document.querySelector(".boardContainer");
+//ARRAY
+const chosenNumbers = [];
+
+//VARIBLE SENTINEL
 
 
-
-function createGame(boardElement, nCell){
+//MAIN JS
+const boardContainer = document.querySelector(".boardContainer")
+function createGame(boardElement, totCell){ //funzione che crea la griglia di gioco
     boardElement.innerHTML = "";
     for (let i = 1; i <= 100; i++){ //ciclo for che va da 1 a 100 
     const boardCell = document.boardCell = document.createElement("div"); //crea l'elemento div
     boardCell.innerHTML = i; // inserisce un valore in ogniuno dei div
     boardCell.classList.add("boardNumber"); // assegna ai div la classe .boardNumber
-    boardCell.addEventListener("click", function(){ //aggiunge un event listener a tutti gli elementi boardCell
-        this.classList.add("selected");
+    boardCell.addEventListener("click", function onClick(){ //aggiunge un event listener a tutti gli elementi boardCell
+        this.classList.add("selected"); //dà agli elemtni delezionati con un click la classe .selected
+        chosenNumbers.push(`${i}`); //inserisce i numeri scelti nell'array chosenNumbers
         console.log(`HAI SELEZIONATO L'ELEMENTO:  ${i}`);
+        console.log(chosenNumbers); //DEBUG
+        if (`${i} == ${chosenNumbers}`){ // ciclo if che fa toglie l'event listener dall'elemento una volta che viene attivato per la prima volta 
+            this.removeEventListener("click", onClick); //rimuove l'event listener
+        }
     });
+    
     boardContainer.append(boardCell); //aggiunge gli elementi nell'html
 }}
 
 document.getElementById(`play`).addEventListener(`click`, function(){
-    const boardContainer = document.querySelector(`.boardContainer`);
-    const totCell = 100;
-    createGame(boardContainer, totCell);
+    const boardContainer = document.querySelector(`.boardContainer`); 
+    const totCell = 100; //indica il numero di celle da creare
+    createGame(boardContainer, totCell); //richiama la funzione che crea la griglia di gioco
 })
+
